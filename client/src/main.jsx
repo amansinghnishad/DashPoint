991082feaa247dccc3c67063b4c9e3c36919a39c
ASN <amansinghnishad0808@gmail.com>
2025-06-18 15:53:11 +0530

    deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Prevent duplicate custom element registration errors
+if (typeof window !== 'undefined' && window.customElements) {
+  const originalDefine = window.customElements.define;
+  window.customElements.define = function(name, constructor, options) {
+    if (!window.customElements.get(name)) {
+      originalDefine.call(this, name, constructor, options);
+    }
+  };
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
